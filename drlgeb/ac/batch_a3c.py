678c12733191fcbc424f086731c4a066797679b3
geb <853934146@qq.com>
2021-01-21 15:16:27 +0800

    delete odd code

--- drlgeb/ac/batch_a3c.py
+++ drlgeb/ac/batch_a3c.py
@@ -30,25 +30,6 @@ class RateSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
         return self.l[-1][1]
 
 
-class DenseAC(tf.keras.Model):
-    def __init__(self, state_size, action_size):
-        super(DenseAC, self).__init__()
-        self.state_size = state_size
-        self.action_size = action_size
-        self.dense1 = tf.keras.layers.Dense(128, activation='relu')
-        self.dense2 = tf.keras.layers.Dense(64, activation='relu')
-
-        self.policy_logits = tf.keras.layers.Dense(action_size)
-        self.values = tf.keras.layers.Dense(1)
-
-    def call(self, inputs):
-        x = self.dense1(inputs)
-        v1 = self.dense2(x)
-        logits = self.policy_logits(x)
-        values = self.values(v1)
-        return logits, values
-
-
 class TransitionExperience(object):
 
     def __init__(self, state, action, reward, **kwargs):
